b1e7b51c55e8ecd96005782e39c6f503e388a2af
Ali Samy <ali.samy@itworxedu.com>
2021-12-28 02:08:18 +0200

    first II

--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.simplesum.BaseActivity
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.e("Ali",sum().toString())
     }
 }
